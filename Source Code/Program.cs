698d73d3b1f76b21148d2625c8a3f8fe67b1827f
Armend <armendbytyqi242003@gmail.com>
2025-03-09 12:46:16 +0100

    Added source code

---
+++ Source Code/Program.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using NLog.Extensions.Logging;
+using Windows_Service;
+
+
+IConfiguration configFile = new ConfigurationBuilder().AddJsonFile("appsettings.json", false , true).Build();
+
+var services = new ServiceCollection();
+
+services
+    .AddLogging(log => { log.ClearProviders(); log.AddNLog(); })
+    .AddSingleton(configFile)
+    .AddScoped<TestLogin>();
+
+using (var serviceProvider = services.BuildServiceProvider())
+{
+    var objekti = serviceProvider.GetRequiredService<TestLogin>();
+    objekti.Shenim();
+}
+
